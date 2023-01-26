8610706d6f7b6f4ee59a63644a4f20409883d523
Bill Bitok <98650586+Billy272@users.noreply.github.com>
2023-01-26 20:38:02 +0300

    Add files via upload

---
+++ Java_Basics/main.java
@@ -0,0 +1,14 @@
+class customer{
+    string name;
+    string email;
+    string region;
+    string country;
+    string accountNumber;
+}
+
+customer C1=new Customer;
+C1.name="Billy";
+C1.email="example@example.com";
+C1.region="Rift Valley";
+C1.country="Kenya";
+C1.accountNumber="B8937HJS839";
