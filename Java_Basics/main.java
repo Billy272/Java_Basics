1367229db101afe6b42acbc97e7d139ccac1aeaa
Bill Cordias <billcordias8@gmail.com>
2023-01-31 22:45:22 +0300

    initial commit

--- Java_Basics/main.java
+++ Java_Basics/main.java
@@ -1,12 +1,12 @@
 class customer{
-    string name;
-    string email;
-    string region;
-    string country;
-    string accountNumber;
-}
+    name: string;
+    email: string;
+    region: string;
+    country: string;
+    accountNumber: string;
 
-customer C1=new Customer;
+}
+customer C1=new Customer();
 C1.name="Billy";
 C1.email="example@example.com";
 C1.region="Rift Valley";
