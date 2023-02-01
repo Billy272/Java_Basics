0d070593831901bd5a1c0f52d0e642dcf12df42d
Bill Cordias <billcordias8@gmail.com>
2023-02-01 22:02:34 +0300

    initial commit

--- Java_Basics/Trade/customer.java
+++ Java_Basics/Trade/customer.java
@@ -1,12 +1,27 @@
 package Trade;
-
+import java.util.Scanner;
 public class customer {
-    public String name;
-    public String email;
-    public double creditLimit;
+    public String Name;
+    public String Email;
+    public double CreditLimit;
 
+    public String getName() {
+        return this.Name;
+    }
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+    public String getEmail(){
+        return this.Email;
+    }
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+    
+    
 //create methods to display
     public void displayDetails(){
-        System.out.println("Name: "+ name+" Email: "+ email+" Credit Limit: "+ creditLimit);
+        System.out.println("Name: "+ Name+" Email: "+ Email+" Credit Limit: "+ CreditLimit);
+        
     }
 }
--- Java_Basics/Trade/main.java
+++ Java_Basics/Trade/main.java
@@ -4,9 +4,9 @@ public class main {
     public static void main(String[] args){
         customer C1 = new customer();
         
-        C1.name = "Bitok";
-        C1.email = "example@example.com";
-        C1.creditLimit = 3000000;
+        C1.setName(" "+name);
+        C1.setEmail(" "+email);
+        C1.CreditLimit = 3000000;
         C1.displayDetails();
     }
     
