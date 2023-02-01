d8c42d89b49682e0cdb85cd225828d5ba0b9231f
Bill Cordias <billcordias8@gmail.com>
2023-02-01 07:59:35 +0300

    initial commit

--- Java_Basics/main.java
+++ Java_Basics/main.java
@@ -5,8 +5,30 @@ class customer{
     country: string;
     accountNumber: string;
 
+@Override
+    protected Object clone() throws CloneNotSupportedException {
+        // TODO Auto-generated method stub
+        return super.clone();
+    }
+
+public customer(customer c1) {
+        C1 = c1;
+    }
+
 }
+public customer(customer c1) {
+        C1 = c1;
+    }
+
 customer C1=new Customer();
+
+public customer getC1() {
+    return C1;
+}
+
+public void setC1(customer c1) {
+    C1 = c1;
+}
 C1.name="Billy";
 C1.email="example@example.com";
 C1.region="Rift Valley";
